31155182f544e860550f2b2d243456e0cca3fd04
metadoks <ufuk.aksel@gmail.com>
2023-12-15 14:41:16 +0300

    tamam

---
+++ ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uaksel <uaksel@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 14:39:00 by uaksel            #+#    #+#             */
+/*   Updated: 2023/12/15 14:39:02 by uaksel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void
+	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		if (*(unsigned char*)(s + i) == (unsigned char)c)
+			return ((void*)(s + i));
+		i++;
+	}
+	return (NULL);
+}
